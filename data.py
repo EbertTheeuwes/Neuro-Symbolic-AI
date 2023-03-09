14bcb01f4a3cb6e8747eba1eefbb9b27caa68c86
EbertTheeuwes <EbertTheeuwes@users.noreply.github.com>
2023-03-10 00:58:29 +0100

    Enkel met 5 en 9 werken

--- data.py
+++ data.py
@@ -12,13 +12,21 @@ transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
 )
 
+full_mnist_trainset = torchvision.datasets.MNIST(root='data/', train=True, download=True, transform=transform)
+full_mnist_testset = torchvision.datasets.MNIST(root='data/', train=False, download=True, transform=transform)
+
+mnist_trainset = []
+for i in range(len(full_mnist_trainset)):
+    if int(full_mnist_trainset[i][1]) == 5 or int(full_mnist_trainset[i][1]) == 9:
+        mnist_trainset += [full_mnist_trainset[i]]
+mnist_testset = []
+for i in range(len(full_mnist_testset)):
+    if int(full_mnist_testset[i][1]) == 5 or int(full_mnist_testset[i][1]) == 9:
+        mnist_testset += [full_mnist_testset[i]]
+
 datasets = {
-    "train": torchvision.datasets.MNIST(
-        root='data/', train=True, download=True, transform=transform
-    ),
-    "test": torchvision.datasets.MNIST(
-        root='data/', train=False, download=True, transform=transform
-    ),
+    "train": mnist_trainset,
+    "test": mnist_testset,
 }
 
 
@@ -46,32 +54,34 @@ class AdditionDataset(Dataset):
         self.dataset = datasets[subset]
 
     def __len__(self):
-        return len(self.dataset) // 6
+        return len(self.dataset) #// 6
 
     def to_query(self, i: int) -> Query:
-        image1 = Term("tensor", Term(self.subset, Constant(i * 6)))
-        image2 = Term("tensor", Term(self.subset, Constant(i * 6 + 1)))
-        image3 = Term("tensor", Term(self.subset, Constant(i * 6 + 2)))
-        image4 = Term("tensor", Term(self.subset, Constant(i * 6 + 3)))
-        image5 = Term("tensor", Term(self.subset, Constant(i * 6 + 4)))
-        image6 = Term("tensor", Term(self.subset, Constant(i * 6 + 5)))
+        image1 = Term("tensor", Term(self.subset, Constant(i)))
+        #image1 = Term("tensor", Term(self.subset, Constant(i * 6)))
+        #image2 = Term("tensor", Term(self.subset, Constant(i * 6 + 1)))
+        #image3 = Term("tensor", Term(self.subset, Constant(i * 6 + 2)))
+        #image4 = Term("tensor", Term(self.subset, Constant(i * 6 + 3)))
+        #image5 = Term("tensor", Term(self.subset, Constant(i * 6 + 4)))
+        #image6 = Term("tensor", Term(self.subset, Constant(i * 6 + 5)))
         #label = Constant(int(self.dataset[i*6][1]))
-        if int(self.dataset[i*6][1]) % 2 == 0:
-            label = Constant(int(0))
-        else:
-            label = Constant(int(1))
+        #if int(self.dataset[i*6][1]) % 2 == 0:
+        #    label = Constant(int(0))
+        #else:
+        #    label = Constant(int(1))
         #label = Constant(int(self.dataset[i*6][1]) + int(self.dataset[i*6 + 1][1]) + int(self.dataset[i*6 + 2][1]))
         #label = Constant(int(self.dataset[i*6][1]) + int(self.dataset[i*6][1]) + int(self.dataset[i*6][1]) +
         #    int(self.dataset[i*6][1]) + int(self.dataset[i*6 + 1][1]) + int(self.dataset[i*6 + 2][1]) +
         #    int(self.dataset[i*6 + 3][1]) + int(self.dataset[i*6 + 4][1]) + int(self.dataset[i*6 + 5][1]))
+        label = Constant(int(self.dataset[i][1]))
         #label = Constant(int(self.dataset[i*6][1]))
         #label = Constant(int(self.dataset[i*6][1] + int(self.dataset[i*6 + 1][1])))
         #term = Term('addition', image1, image2, label)
         #term = Term('check', [[image1,image1,image1],[image1,image2,image3],[image4,image5,image6]], label)
-        term = Term('check', image1, image1, image1, image1, image2, image3, image4, image5, image6, label)
+        #term = Term('check', image1, image1, image1, image1, image2, image3, image4, image5, image6, label)
         #term = Term('add', image1, image1, image1, image1, image2, image3, image4, image5, image6, label)
         #term = Term('evenOrOdd', image1, label)
-        #term = Term('idd', image1, label)
+        term = Term('idd', image1, label)
         #term = Term('add3', image1, image2, image3, label)
         #term = Term('labelnum', image1, label)
         #print(image1)
